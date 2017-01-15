82c3873193ab06000f1002378765dd3166bcffc9
trungqhoang <tqhoang@outlook.com>
2017-01-14 18:47:26 -0800

    Update EntityList.h

--- Internal/Internal/EntityList.h
+++ Internal/Internal/EntityList.h
@@ -8,7 +8,7 @@
 
 struct EntityList {
 	DWORD *base;		// Base address of this entity
-	int *team;			// The team of this entity
+	int *team;		// The team of this entity
 	int *health;		// The current health of this entity
 	int *spotted_mask;	// A 32 bit number representing other entities this entity is visible to
 	bool *dormant;		// Whether or not this entity is dormant
