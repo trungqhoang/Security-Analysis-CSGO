66b948d1c94f14c8399be40a4f66c6f829c038bf
trungqhoang <tqhoang@outlook.com>
2017-01-14 19:33:32 -0800

    Update LocalPlayer.h

--- External/External/LocalPlayer.h
+++ External/External/LocalPlayer.h
@@ -7,15 +7,15 @@
 */
 
 struct LocalPlayer {
-	DWORD base;				// Base address in memory of the player
-	int team;				// The team number the player
+	DWORD base;			// Base address in memory of the player
+	int team;			// The team number the player
 	int crosshair_id;		// The player's current target
-	int target;				// The player's current target
+	int target;			// The player's current target
 	int shots_fired;		// The number of bullets fired by the player
 	float punch[2];			// The amount of recoil applied to the player
 	float position[3];		// The position of the player
 
 	DWORD client_state;		// Base address of the client state
 	int local_index;		// Index of the player with respect to the entity list
-	float view_angles[3];	// The current view angles of the player
+	float view_angles[3];		// The current view angles of the player
 };
